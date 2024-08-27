6870e68f55fca95ef0c40480fd56f8193303ff02
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-08-27 16:26:55 +0600

    painting colored

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import colorgram as cg
+import turtle as t
+import random
 
-colors = cg.extract('spot-paint.webp', 6)
-rgb_colors = []
+# rgb_colors = []
+# colors = cg.extract('spot-paint.webp', 6)
 
-for i in colors:
-    red = i.rgb.r
-    green = i.rgb.r
-    blue = i.rgb.b
-    rgb_tuple = (red, green, blue)
+# for i in colors:
+#     red = i.rgb.r
+#     green = i.rgb.r
+#     blue = i.rgb.b
+#     rgb_tuple = (red, green, blue)
 
-    rgb_colors.append(rgb_tuple)
+#     rgb_colors.append(rgb_tuple)
 
-print(colors)
+rgb_colors = [(233, 233, 232), (231, 231, 237), (235, 235, 233), (224, 224, 227), (207, 207, 82), (54, 54, 130)]
+
+t.colormode(255)
+turtle_obj = t.Turtle()
+turtle_obj.setheading(180)
+
+for i in range (10):
+    turtle_obj.dot(20, random.choice(rgb_colors))
+    turtle_obj.forward(50)
+
+
+
+
+
+
+screen_obj = t.Screen()
+screen_obj.exitonclick()
