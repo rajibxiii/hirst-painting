074edfe9343552e0017658fb42a34a6be7e0f66b
muhammad_rajib <48186532+rajibxiii@users.noreply.github.com>
2024-08-27 16:47:45 +0600

    perfectl working dots

--- main.py
+++ main.py
@@ -17,14 +17,23 @@ rgb_colors = [(233, 233, 232), (231, 231, 237), (235, 235, 233), (224, 224, 227)
 
 t.colormode(255)
 turtle_obj = t.Turtle()
-turtle_obj.setheading(180)
+turtle_obj.speed("fastest")
+turtle_obj.setheading(225)
+turtle_obj.forward(300)
+turtle_obj.setheading(0)
 
-for i in range (10):
+numberOfDots = 50
+
+for i in range (1, numberOfDots+1):
     turtle_obj.dot(20, random.choice(rgb_colors))
     turtle_obj.forward(50)
 
-
-
+    if i % 10 == 0:
+        turtle_obj.setheading(90)
+        turtle_obj.forward(50)
+        turtle_obj.setheading(180)
+        turtle_obj.forward(500)
+        turtle_obj.setheading(0)
 
 
 
